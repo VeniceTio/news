45569245084fec29b3e490e011148a8f0ea0185d
mankoty567 <mankoty567@outlook.fr>
2020-06-29 11:55:29 +0200

    Correction sur la directive shareOnTwitter

--- js/directive/NewsShareOnTwitter.js
+++ js/directive/NewsShareOnTwitter.js
@@ -3,9 +3,8 @@
 app.directive('newsShareOnTwitter', function ($rootScope, $timeout) {
 
     return {
-        function (scope, elem) {
-            $rootScope.$on('shareOnTwitter', function (events, args) {
-
+        link : function (scope, elem) {
+            $rootScope.$on('shareOnTwitter', function () {
                 console.log("Un essai");
             });
         }
