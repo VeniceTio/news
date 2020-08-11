93314b0cd14016f6815eb60a2f64910434178675
Venice <yoann.louazel@gmail.com>
2020-08-11 10:56:12 +0200

    modification count article

--- templates/part.navigation.shared.php
+++ templates/part.navigation.shared.php
@@ -13,25 +13,8 @@
             <li class="app-navigation-entry-utils-counter"
                 ng-show="Navigation.isSharedUnread()"
                 title="{{ Navigation.getSharedCount() }}">
-                {{ Navigation.getUnreadCount() | unreadCountFormatter }}
-            </li>
-            <li class="app-navigation-entry-utils-menu-button">
-                <button
-                    ng-click="optionsId = (optionsId == 'all' ? -1 : 'all')">
-                </button>
+                {{ Navigation.getSharedCount() | unreadCountFormatter }}
             </li>
         </ul>
     </div>
-
-    <div class="app-navigation-entry-menu">
-        <ul>
-            <li class="mark-read">
-                <button ng-click="Navigation.markRead()">
-                    <span class="icon-checkmark"></span>
-                    <span><?php p($l->t('Mark read')); ?></span>
-                </button>
-            </li>
-        </ul>
-    </div>
-
 </li>
